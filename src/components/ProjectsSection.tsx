23fdf31a4b7acd23156cbe58119147a477e8e343
John Pacheco <99419989+JPacheco35@users.noreply.github.com>
2026-03-21 16:43:17 -0400

    add: spun off projects as its own Projects.ts

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -3,74 +3,7 @@ import { Card, CardContent, CardTitle } from '@/components/ui/card'
 import styles from "@/components/HeroSection.module.css";
 import { SiGithub } from 'react-icons/si'
 import { GlobeIcon } from 'lucide-react'
-
-import pheelsDemoImage from '../assets/images/pheels.png'
-import truncc8DemoImage from '../assets/images/truncc8.png'
-import dobleseisDemoImage from '../assets/images/dobleseis.png'
-import portfolioDemoImage from '../assets/images/portfolio.png'
-
-interface Project {
-  id: string
-  title: string
-  category: string
-  image: string
-  description: string
-  technologies: string[]
-  links: { label: string; href: string; iconType: 'github' | 'demo' | 'docs' }[]
-}
-
-const projects: Project[] = [
-  {
-    id: '1',
-    title: 'Truncc8',
-    category: 'Website',
-    image: truncc8DemoImage,
-    description:
-      'Truncc8 is a URL shortener built with TypeScript, Express, and MongoDB.',
-    technologies: ['React', 'Node.js', 'MongoDB', 'Express', 'MantineUI'],
-    links: [
-      { label: 'GitHub', href: 'https://github.com/JPacheco35/truncc8', iconType: 'github' },
-      { label: 'Live Demo', href: 'https://truncc8.vercel.app/', iconType: 'demo' },
-    ],
-  },
-  {
-    id: '2',
-    title: 'Pheels',
-    category: 'Web Application',
-    image: pheelsDemoImage,
-    description:
-      'A journaling and mood diary web app written in TypeScript using the MERN stack.',
-    technologies: ['React', 'Node.js', 'MongoDB', 'Express', 'MantineUI'],
-    links: [
-      { label: 'GitHub', href: 'https://github.com/JPacheco35/truncc8', iconType: 'github' },
-      { label: 'Live Demo', href: 'https://truncc8.vercel.app', iconType: 'docs' },
-    ],
-  },
-  {
-    id: '3',
-    title: 'Doble-Seis',
-    category: 'Browser Game',
-    image: dobleseisDemoImage,
-    description: 'A 2v2 multiplayer browser dominoes team game built with the MERN stack, featuring real-time matches, a matchmaking system, and smooth interactive gameplay.',
-    technologies: ['React', 'Node.js', 'MongoDB', 'Express', 'MantineUI'],
-    links: [
-      { label: 'GitHub', href: 'https://github.com/JPacheco35/doble-seis', iconType: 'github' },
-      { label: 'Live Demo', href: 'https://doble-seis.vercel.app', iconType: 'docs' },
-    ],
-  },
-  {
-    id: '4',
-    title: 'Portfolio Website',
-    category: 'Website',
-    image: portfolioDemoImage,
-    description: 'Portfolio website with experience history, skillset, contact form, and project showcase.',
-    technologies: ['React', 'TypeScript', 'Tailwind CSS', 'Shadcn-UI'],
-    links: [
-      { label: 'GitHub', href: 'https://github.com/JPacheco35/portfolio', iconType: 'github' },
-      { label: 'Live Demo', href: 'https://johnpacheco.org', iconType: 'docs' },
-    ],
-  }
-]
+import {projects} from "@/components/Projects.ts";
 
 export function ProjectsSection() {
 
